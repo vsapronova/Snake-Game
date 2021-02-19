c0741ab476ee9a0d50eb95558eee06893ebbd2cd
Victoria Sapronova <victoriasapronova@Victorias-MBP.fios-router.home>
2021-02-19 11:57:02 -0500

    First commit

---
+++ theme.js
@@ -0,0 +1,21 @@
+
+
+
+export default {
+    colorPrimary: "#8739F9",
+	colorAccent: "#37B9F1",
+	colorGrey: "#F2F5F5",
+	colorText: "#565360",
+	colorLabel: "#908E9B",
+	colorDisable: "#E1DFE9",
+	lengthSm1: "0.25rem",
+	lengthSm2: "0.5rem",
+    lengthSm3: "0.75rem",
+    lengthMd1: "1rem",
+	lengthMd2: "1.25rem",
+	lengthMd3: "1.5rem",
+    lengthLg1: "2rem",
+	lengthLg2: "3rem",
+	lengthLg3: "4rem"
+
+}
